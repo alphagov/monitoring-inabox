915fd84622103a3e0fd70d5efbae438ee583f7e5
Philip Potter <philip.g.potter@gmail.com>
2013-06-28 17:33:05 +0100

    experimental gstatsd module

---
+++ modules/gds_statsd/manifests/init.pp
@@ -0,0 +1,8 @@
+class gds_statsd {
+  ensure_packages(['python-dev','python-pip'])
+  Package [['python-dev'],['python-pip']] ->
+  class {'gstatsd':
+    graphite_server => '172.16.10.10',
+    flush_interval  => 10,
+  }
+}
