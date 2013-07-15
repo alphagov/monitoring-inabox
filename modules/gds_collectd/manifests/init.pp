c10c0dd1b1c0341ba0feecf35bc8b766535f2bcf
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2013-07-15 17:32:06 +0100

    Added collectd config to control plugins and transmit stats
    the flags on the collectd class ensure that you get the plugins
    you specify not the ones in the default conf file.
    
    This then allows the specification of a plugin to send data to
    to carbon on the graphite server, log to syslog and to collect
    baseline stats.

--- modules/gds_collectd/manifests/init.pp
+++ modules/gds_collectd/manifests/init.pp
@@ -1,5 +1,16 @@
 class gds_collectd {
-  include collectd
-  include collectd::plugin::write_graphite
+  class { '::collectd':
+    purge        => true,
+    recurse      => true,
+    purge_config => true,
+  }
+
+  class { 'collectd::plugin::write_graphite':
+    graphitehost => '172.16.10.10',
+  }
+
+  collectd::plugin {'syslog':}
+  collectd::plugin {'memory':}
   collectd::plugin {'cpu':}
+  collectd::plugin {'interface':}
 }
