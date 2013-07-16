89194545ce2a86eac17bd0a6fef9ce5751e57bb6
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2013-07-16 12:10:13 +0100

    puppet script tidy up and additional comments

--- modules/gds_collectd/manifests/init.pp
+++ modules/gds_collectd/manifests/init.pp
@@ -1,5 +1,11 @@
+# collectd installation
+#
+# collect metrics on cpu, memory and network
+# log to syslog at 'info' level
+# send data collected to graphite on the monitoring box
+#
 class gds_collectd {
-  class { '::collectd':
+  class { 'collectd':
     purge        => true,
     recurse      => true,
     purge_config => true,
