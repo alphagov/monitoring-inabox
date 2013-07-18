55e4c9688580fbd604c95376e03dcc6cb5bdb498
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2013-07-18 15:01:50 +0100

    replay commit erroneously backed out
    cc @danielabel

--- modules/gds_collectd/manifests/init.pp
+++ modules/gds_collectd/manifests/init.pp
@@ -5,8 +5,18 @@
 # send data collected to graphite on the monitoring box
 #
 class gds_collectd {
-  include collectd
-  include collectd::plugin::write_graphite
+  class { 'collectd':
+    purge        => true,
+    recurse      => true,
+    purge_config => true,
+  }
 
+  class { 'collectd::plugin::write_graphite':
+    graphitehost => '172.16.10.10',
+  }
+
+  collectd::plugin {'syslog':}
+  collectd::plugin {'memory':}
   collectd::plugin {'cpu':}
+  collectd::plugin {'interface':}
 }
